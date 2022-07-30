6d4f9a8b9d6d9aa564dcdb4d6a601e8fe2e3354c
rzkfyn <rzkfyn@gmail.com>
2022-07-30 14:43:47 +0700

    Search anime handler

---
+++ handler/handler.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import otakudesu from '../src/otakudesu.js';
+import { searchResultAnime } from '../src/types/types.js';
+
+const searchAnimeHandler = async (req: Request, res: Response) => {
+  const { keyword } = req.params;
+
+  let data: searchResultAnime[];
+  try{
+    data = await otakudesu.search(keyword);
+  } catch(e) {
+    console.log(e);
+    return res.status(500).json({
+      status: 'Error',
+      message: 'Internal server error'
+    });
+  }
+
+  return res.status(200).json({
+    status: 'Ok',
+    message: 'Otakudesu unofficial api, made by rzkfyn with <3',
+    data
+  });
+}
+
+export default {
+  searchAnimeHandler
+}
