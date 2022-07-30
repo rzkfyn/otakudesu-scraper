12aa0555a2dc75986e9f46872ea89409d6289951
rzkfyn <rzkfyn@gmail.com>
2022-07-30 14:46:52 +0700

    Menggunakan cors dan konfigurasi route

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import routes from './routes/routes.js';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello, World'));
+app.use(cors());
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}, http://localhost:${port}`);
