9b2df03ec6bc5b128d5fc82e6a4c967fd4f7be6a
rzkfyn <rzkfyn@gmail.com>
2022-07-31 15:05:04 +0700

    Home handler

--- handler/handler.ts
+++ handler/handler.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import otakudesu from '../src/otakudesu.js';
-import { searchResultAnime } from '../src/types/types.js';
+import { ongoingAnime, searchResultAnime } from '../src/types/types.js';
 
 const searchAnimeHandler = async (req: Request, res: Response) => {
   const { keyword } = req.params;
@@ -23,6 +23,26 @@ const searchAnimeHandler = async (req: Request, res: Response) => {
   });
 }
 
+const homeHandler = async (_: Request, res: Response)  => {
+  let data: { ongoingAnime: ongoingAnime[] };
+  try {
+    data = await otakudesu.home();
+  } catch(e) {
+    console.log(e);
+    return res.status(500).json({
+      status: 'Error',
+      message: 'Internal server error'
+    });
+  }
+
+  return res.status(200).json({
+    status: 'Ok',
+    message: 'Otakudesu unofficial api, made by rzkfyn with <3',
+    data
+  });
+}
+
 export default {
-  searchAnimeHandler
+  searchAnimeHandler,
+  homeHandler
 }
