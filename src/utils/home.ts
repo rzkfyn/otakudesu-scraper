cf3b22493c3c53e2e745c0406a9a18cce0094dc4
rzkfyn <rzkfyn@gmail.com>
2022-08-01 11:30:47 +0700

    menambah data complete anime di path home

--- src/utils/home.ts
+++ src/utils/home.ts
@@ -2,16 +2,20 @@ import axios from 'axios';
 import { load } from 'cheerio';
 import { BASEURL } from '../../config.js';
 import scrapeOngoingAnime from '../lib/scrapeongoinganime.js';
+import scrapeCompleteAnime from '../lib/scrapecompleteanime.js';
 import { ongoingAnime as ongoingAnimeType } from '../types/types.js';
 
-const home = async (): Promise<{ ongoingAnime: ongoingAnimeType[] }> => {
+const home = async (): Promise<{ ongoingAnime: ongoingAnimeType[], completeAnime: any[] }> => {
   const { data } = await axios.get(BASEURL)
   const $ = load(data);
-  const ongoingAnimeEls = $('.venutama .rseries .rapi .venz ul li').toString();
+  const ongoingAnimeEls = $('.venutama .rseries .rapi:first .venz ul li').toString();
+  const completeAnimeEls = $('.venutama .rseries .rapi:last .venz ul li').toString();
   const ongoingAnime = scrapeOngoingAnime(ongoingAnimeEls);
+  const completeAnime = scrapeCompleteAnime(completeAnimeEls);
 
   return {
-    ongoingAnime
+    ongoingAnime,
+    completeAnime
   };
 }
 
