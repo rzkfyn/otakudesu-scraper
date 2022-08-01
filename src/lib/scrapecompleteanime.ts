523a7fe2a86c0d25c2cdb939bbd9b7e6505e1f9f
rzkfyn <rzkfyn@gmail.com>
2022-08-01 11:30:05 +0700

    scrape complete anime from home

---
+++ src/lib/scrapecompleteanime.ts
@@ -0,0 +1,27 @@
+import { load } from 'cheerio';
+import { BASEURL } from '../../config.js';
+
+const scrapeCompleteAnime = (html: string) => {
+  const result: any[] = [];
+  const animes = html.split('</li>')
+  .filter(item => item.trim() !== '')
+  .map(item => `${item}</li>`);
+
+  animes.forEach(anime => {
+    const $ = load(anime);
+    
+    result.push({
+      title: $('.detpost .thumb .thumbz .jdlflm').text(),
+      slug: $('.detpost .thumb a').attr('href')?.replace(`${BASEURL}/anime/`, '').replace('/', ''),
+      poster: $('.detpost .thumb .thumbz img').attr('src'),
+      episodeCount: $('.detpost .epz').text().trim(),
+      rating: $('.detpost .epztipe').text().trim(),
+      lastReleaseDate: $('.detpost .newnime').text(),
+      url: $('.detpost .thumb a').attr('href')
+    });
+  });
+
+  return result;
+}
+
+export default scrapeCompleteAnime;
