accee53c90149ba971b580a77bad37df894304d4
rzkfyn <rzkfyn@gmail.com>
2022-08-05 19:17:23 +0700

    fungsi untuk mengambil data pagination

---
+++ src/lib/pagination.ts
@@ -0,0 +1,32 @@
+import { load } from 'cheerio';
+
+const pagination = (html: string): false | {
+  currentPage: number;
+  lastPage: number;
+  isHasNextPage: boolean;
+  nextPage: number | null;
+  isHasPreviousPage: boolean;
+  previousPage: number | null;
+} => {
+  const $ = load(html);
+
+  const currentPage = parseInt($('.pagination .pagenavix .page-numbers.current').text());
+  const lastPage = parseInt($('.pagination .pagenavix .page-numbers:last').prev('a.page-numbers').text());
+  const nextPage = currentPage < lastPage ? currentPage + 1 : null;
+  const previousPage = currentPage > 1 ? currentPage - 1 : null;
+  const isHasNextPage = currentPage < lastPage;
+  const isHasPreviousPage = currentPage > 1;
+
+  if (!currentPage) return false;
+
+  return {
+    currentPage,
+    lastPage,
+    isHasNextPage,
+    nextPage,
+    isHasPreviousPage,
+    previousPage    
+  }
+}
+
+export default pagination;
