a0ed3b5a2ac9f8b2f9d334a4e3f3fa170d0e2e50
rzkfyn <rzkfyn@gmail.com>
2022-08-05 19:18:07 +0700

    ongoing anime handler

--- handler/handler.ts
+++ handler/handler.ts
@@ -42,7 +42,43 @@ const homeHandler = async (_: Request, res: Response)  => {
   });
 }
 
+const ongoingAnimeHandler = async (req: Request, res: Response) => {
+  const { page } = req.params;
+  if (page) {
+    if (!parseInt(page)) return res.status(400).json({
+      status: 'Errpr',
+      message: 'The page parameter must be a number!'
+    });
+  }
+  
+  let result;
+  try {
+    result = page ? await otakudesu.ongoingAnime(parseInt(page)) : await otakudesu.ongoingAnime();
+  } catch(e) {
+    console.log(e);
+    return res.status(500).json({
+      status: 'Error',
+      message: 'Internal server error'
+    });
+  }
+
+  const { paginationData, ongoingAnimeData } = result;
+
+  if (!paginationData) return res.status(404).json({
+    status: 'Error',
+    message: 'There\'s nothing here ;_;'
+  });
+
+  return res.status(200).json({
+    status: 'Ok',
+    message: 'Otakudesu unofficial api made by rzkfyn with <3',
+    ...paginationData,
+    data: ongoingAnimeData
+  });
+}
+
 export default {
   searchAnimeHandler,
-  homeHandler
+  homeHandler,
+  ongoingAnimeHandler
 }
